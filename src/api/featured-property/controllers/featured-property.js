f492580c3e1df43b33fe1b4a9e025a3fe2584414
Miggy Anover <miggyanover@gmail.com>
2023-02-21 11:25:06 +0800

    Git init

---
+++ src/api/featured-property/controllers/featured-property.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * featured-property controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::featured-property.featured-property');
---
+++ src/api/featured-property/routes/featured-property.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * featured-property router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::featured-property.featured-property');
